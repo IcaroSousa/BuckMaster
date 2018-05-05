9b8991427961f1d380d015ff032215770b8539fb
Icaro Sousa <IcaroSousa@users.noreply.github.com>
2018-05-05 00:21:09 -0300

    Update SqlServerDb.py

--- src/Database/SqlServerDb.py
+++ src/Database/SqlServerDb.py
@@ -5,7 +5,7 @@ class SqlServerDb(ConnectionBase.ConnectionBase):
 
     def __init__(self):
         self._DBDriver = "{ODBC Driver 17 for SQL Server}"
-        super().__init__("smooth.database.windows.net", 1433, "Rockfeller", "icaro", "#$Attack91")
+        super().__init__("smooth.database.windows.net", 1433, "Rockfeller", "icaro", "")
 
     def executeQuery(self, pQuery):
         self._Query.execute(pQuery)
